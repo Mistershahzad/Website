52b26c17e643d07a52f0ece8cae1b25ad193a560
Mister shah <shazadxaleem@gmail.com>
2022-11-02 01:42:28 +0500

    Data types

---
+++ Referance.js
@@ -0,0 +1,35 @@
+// Ways to print in JavaScript
+Javascript console API 
+JavaScript Variables
+
+Data types in JavaScript..
+String
+Objects
+Booleans
+
+// 1. Primitive data types: undefined, null, number, string, boolean, symbol
+// 2. Reference data types: Arrays and Objects
+// Operators in JavaScript
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// Logical Operators
+// DRY
+// Conditionals in JavaScript
+// Single if statement
+// if - else statement
+// else
+// if-else Ladder
+// else
+// Array Methods
+// String Methods in JavaScript
+// DOM Manipulation
+// removeChild(element)
+// Selecting using Query
+// function clicked
+// Events in JavaScript
+// Arrow Functions
+// SetTimeout and setinterval
+// JavaScript localStorage
+// Json
+// Template literals - Backticks
